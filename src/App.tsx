04c4ba286f8659a82e45eebb74cd4339475faa8a
virtutae <virtutae@yahoo.it>
2023-08-15 16:36:34 +0100

    added AppHeader, InputBox, ListOfItems

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
-import { greet } from "./utils/greet";
+import AppHeader from "./components/AppHeader";
+
+
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  
+  
+  return( 
+  <>
+    <AppHeader/>
+    <InputBox/>
+    <ListOfItems/>
+  </>
+  
+  )
 }
 
 export default App;
+
+ 
+function InputBox():JSX.Element{
+  // const [inputValue, setInputValue] = useState('');
+
+
+return (
+  <>
+    <input type="text" className="input-bar" placeholder="Add the task here"/>
+    <button type="button" className="add-button">Add item</button>
+  </>
+
+)
+}
+
+function ListOfItems (): JSX.Element {
+  return (
+    <>
+    </>
+  )
+}
